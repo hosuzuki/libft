77740311aa1ac230f042da218064ea8c29b56a83
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2021-11-14 20:52:33 +0900

    before modification

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,18 +6,18 @@
 /*   By: hokutosuzuki <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 18:03:49 by hokutosuz         #+#    #+#             */
-/*   Updated: 2021/11/05 11:32:43 by hokutosuz        ###   ########.fr       */
+/*   Updated: 2021/11/14 20:52:06 by hokutosuz        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void	*(*f)(void*), void	(*del)(void*))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void*))
 {
 	t_list	*n;
 	t_list	*tmp;
 
-	if (!lst || !f)
+	if (!lst || !f || !del)
 		return (NULL);
 	n = ft_lstnew(f(lst->content));
 	if (n == NULL)
