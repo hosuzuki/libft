f01252910a295e85413bc31570848cbe9fc3e65a
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2021-11-15 12:36:51 +0900

    experiment

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,11 +6,12 @@
 /*   By: hokutosuzuki <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 18:03:49 by hokutosuz         #+#    #+#             */
-/*   Updated: 2021/11/15 10:57:22 by hokutosuz        ###   ########.fr       */
+/*   Updated: 2021/11/15 12:19:50 by hokutosuz        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 /*
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
@@ -33,6 +34,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 }
 */
 
+#include <stdio.h>
+
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
@@ -40,11 +43,32 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if ((unsigned char *)(s1 + i) > (unsigned char *)(s2 + i))
+		printf("\ns1 is : %c\ns2 is : %c\n", (unsigned char *)s1[i], (unsigned char)s2[i]);
+		if ((unsigned char)s1[i] > (unsigned char)s2[i])
 			return (1);
-		else if ((unsigned char *)(s1 + i) < (unsigned char *)(s2 + i))
+		else if ((unsigned char)s1[i] < (unsigned char)s2[i])
 			return (-1);
 		i++;
 	}
 	return (0);
 }
+/*
+#include <stdio.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("\ns1 is : %c\ns2 is : %c\n", *(unsigned char *)(s1 + i), *(unsigned char *)(s2 + i));
+		if (*(unsigned char *)(s1 + i) > *(unsigned char *)(s2 + i))
+			return (1);
+		else if (*(unsigned char *)(s1 + i) < *(unsigned char *)(s2 + i))
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
