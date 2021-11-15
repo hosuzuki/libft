bc0481259fbf6590e79263f7a5fa28ba589d873b
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2021-11-15 10:57:49 +0900

    simplified

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,13 +6,13 @@
 /*   By: hokutosuzuki <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 18:03:49 by hokutosuz         #+#    #+#             */
-/*   Updated: 2021/11/05 13:22:55 by hokutosuz        ###   ########.fr       */
+/*   Updated: 2021/11/15 10:57:22 by hokutosuz        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int	ft_memcmp(const void	*s1, const void	*s2, size_t	n)
+/*
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*str1;
 	unsigned char	*str2;
@@ -23,8 +23,27 @@ int	ft_memcmp(const void	*s1, const void	*s2, size_t	n)
 	str2 = (unsigned char *)s2;
 	while (i < n)
 	{
-		if (str1[i] != str2[i])
-			return (str1[i] - str2[i]);
+		if (str1[i] > str2[i])
+			return (1);
+		else if (str1[i] < str2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+*/
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if ((unsigned char *)(s1 + i) > (unsigned char *)(s2 + i))
+			return (1);
+		else if ((unsigned char *)(s1 + i) < (unsigned char *)(s2 + i))
+			return (-1);
 		i++;
 	}
 	return (0);
