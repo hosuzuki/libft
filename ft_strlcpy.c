93afb526837305fc6aa0c2336954d6894a693c7a
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2021-11-16 15:20:02 +0900

    minor modification

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,29 +6,26 @@
 /*   By: hokutosuzuki <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 18:03:51 by hokutosuz         #+#    #+#             */
-/*   Updated: 2021/11/03 15:13:56 by hokutosuz        ###   ########.fr       */
+/*   Updated: 2021/11/16 15:19:32 by hokutosuz        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char	*dst, const char	*src, size_t	dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t	n;
+	size_t	i;
 	size_t	c;
 
-	n = 0;
-	c = 0;
-	while (src[c])
-		c++;
-	if (dstsize > 0)
+	i = 0;
+	c = ft_strlen(src);
+	if (dstsize <= 0)
+		return (c);
+	while (src[i] != '\0' && i + 1 < dstsize)
 	{
-		while (src[n] != '\0' && n < (dstsize - 1))
-		{
-			dst[n] = src[n];
-			n++;
-		}
-		dst[n] = '\0';
+		dst[i] = src[i];
+		i++;
 	}
+	dst[i] = '\0';
 	return (c);
 }
