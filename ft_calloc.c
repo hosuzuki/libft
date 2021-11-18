449ebeaac8912cfe9731e53702b01ef574028282
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2021-11-19 07:09:33 +0900

    modification

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hokutosuzuki <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 18:03:48 by hokutosuz         #+#    #+#             */
-/*   Updated: 2021/11/19 06:57:14 by hokutosuz        ###   ########.fr       */
+/*   Updated: 2021/11/19 07:08:40 by hokutosuz        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	char	*m;
 
-	if (count == 0 || size == 0);
-		return (ft_strdup("");
+	if (count == 0 || size == 0)
+		return (ft_strdup(""));
 	m = malloc(count * size);
 	if (!m)
 		return (NULL);
--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hokutosuzuki <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 18:03:51 by hokutosuz         #+#    #+#             */
-/*   Updated: 2021/11/16 12:39:08 by hokutosuz        ###   ########.fr       */
+/*   Updated: 2021/11/19 07:09:03 by hokutosuz        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,10 @@ char	*ft_strdup(const char *s1)
 	size_t	i;
 	char	*dst;
 
-	i = 0;
-	dst = ft_calloc(ft_strlen(s1) + 1, sizeof(char));
+	i = ft_strlen(s1);
+	dst = ft_calloc(i + 1, sizeof(char));
 	if (!dst)
 		return (NULL);
-	while (s1[i])
-	{
-		dst[i] = s1[i];
-		i++;
-	}
-	dst[i] = '\0';
+	ft_strlcpy(dst, s1, i);
 	return (dst);
 }
